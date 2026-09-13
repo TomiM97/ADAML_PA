083ef9e9a8ca84d5dfb3e381e359f30884b065e2
jonnaviljakainen <Jonna.Viljakainen@student.lut.fi>
2026-09-13 12:42:58 +0300

    Update PW_PCA.m
    Kaikki nyt samassa

--- PW_PCA.m
+++ PW_PCA.m
@@ -45,3 +45,32 @@ legend({'maximum', 'mean', 'minimum'},'FontSize',12)
 
 %% PCA
 [loadings, scores, latent, tsqrd, explained] = pca(X);
+
+% PC1-PC2 scores
+figure;
+biplot(loadings(1:15:end,1:2),'Scores', scores(1:15:end,1:2))
+xlabel('PC1 score');
+ylabel('PC2 score');
+legend('Coeff 1', 'Coeff 2', '', 'Scores')
+grid on;
+% PC1-PC3 loadings
+figure
+plot(wavelenghts, loadings(:,1:3), 'LineWidth', 1.2);
+xlabel('Wavelength (nm)');
+ylabel('Loading');
+
+legend('PC1','PC2','PC3');
+xlim([399 2501]);
+grid on;
+% Explained variance
+figure;
+pareto(explained);
+xlabel('Principal component');
+ylabel('Explained variance (%)');
+grid on;
+% PC2-PC3 scores
+figure;
+plot(scores(1:15:end,2), scores(1:15:end,3), '.');
+xlabel('PC2 score');
+ylabel('PC3 score');
+grid on;
